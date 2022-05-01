4cfe75ce8785bcf483d4997b0d7955c668ac54e3
CikguAbdulRahman <104701677+CikguAbdulRahman@users.noreply.github.com>
2022-05-01 09:28:00 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+images.createBigImage(`
+    . # # . . . . . . .
+    # # # . # # # . # #
+    . . # . # . # . # .
+    . . # # # . # # # .
+    # # # # # # # # # #
+    `).scrollImage(1, 500)
+basic.clearScreen()
+basic.showString("5Inovatif")
+basic.forever(function () {
+    basic.showString("Matematik")
+    basic.clearScreen()
+    basic.showString("Khamis")
+    basic.clearScreen()
+    basic.showString("10.20pagi-11.20pagi")
+})
